becccafdb7320fde09972442e775edd7838cc4ba
vivaldifikry <vivaldifikry2@gmail.com>
2024-03-04 21:44:59 +0700

    initial commit

---
+++ gen-20-java-basic/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {         //nama kelas
+    public static void main(String[] args) {        //main method void
+
+        System.out.println("Hello World!");          //output hello world
+
+    }
+}
